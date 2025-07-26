c622983f13ed9a8eea57e3030473a75ccf9dca68
Hira703 <soniahira48@gmail.com>
2025-07-26 13:38:29 +0600

    updated

--- server/config/firebase-admin.js
+++ server/config/firebase-admin.js
@@ -1,14 +1,6 @@
 const admin = require("firebase-admin");
 
-let serviceAccount;
-
-if (process.env.FB_SERVICE_KEY) {
-  // Directly parse the JSON string from the env variable
-  serviceAccount = JSON.parse(process.env.FB_SERVICE_KEY);
-} else {
-  // Fallback for local development using the JSON file
-  serviceAccount = require("../firebase-admin.json");
-}
+const serviceAccount = JSON.parse(process.env.FB_SERVICE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
